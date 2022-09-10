36c91e76af0eab714f055cbef386064e9f2527e2
Mehrdad Arman <mehrdad0011998@gmail.com>
2022-09-10 21:33:14 +0430

    Drawing method was changed to typewriter style, args parsing added

--- pacmanfetch.py
+++ pacmanfetch.py
@@ -10,85 +10,134 @@
 # Pacman_Fetch
 
 __repo__ = "https://github.com/mehrdad-mixtape/Pacman_Fetch"
-__version__ = "v0.1.1"
+__version__ = "v0.4.0"
 
 """ Pacman Fetch!
 For Better Experiance Install icon-in-terminal:
 Github repo: https://github.com/sebastiencs/icons-in-terminal """
 
-from typing import List
+from typing import List, Dict, Generator
 from time import sleep, time
 from rich.console import Console
 from random import shuffle, choice
+from argparse import ArgumentParser
 import os, platform, subprocess, \
-    re, sys, distro, psutil, GPUtil
+    re, sys, distro, psutil
 
 # Blocks
+# -------------------------------------------------------------------
 F = "█"
 E = "▒"
+
+# Height and Width of Pacman & Ghost
+# -------------------------------------------------------------------
 W = 29
 H = 12
 
+# length of separator
+# -------------------------------------------------------------------
 D = 0
 
-BANNER = """┌─────────────────┐
-               │  Pacmanfetch  │
-               └─────────────────┘"""
-
-# Colorful blocks
-BW = f"[white]{F}[/white]"
-BC = f"[cyan]{F}[/cyan]"
-BY = f"[gold1]{F}[/gold1]"
-BG = f"[chartreuse3]{F}[/chartreuse3]"
-BR = f"[red]{F}[/red]"
-BP = f"[purple]{F}[/purple]"
-BO = f"[dark_orange]{F}[/dark_orange]"
-BK = f"[black]{F}[/black]"
+MAIN_BANNER = """[blink]┌───────────────────┐
+           │   Pacmanfetch   │
+           └───────────────────┘
+[/blink]"""
 
 # Color list
+# -------------------------------------------------------------------
 colors: List[str] = [
-    "[red]{}[/red]",
-    "[purple]{}[/purple]",
-    "[dark_orange]{}[/dark_orange]",
-    "[chartreuse3]{}[/chartreuse3]",
-    "[cyan]{}[/cyan]",
-    "[hot_pink]{}[/hot_pink]",
-    "[orange4]{}[/orange4]",
-    "[dark_cyan]{}[/dark_cyan]",
-    "[grey74]{}[/grey74]",
-    "[slate_blue3]{}[/slate_blue3]",
-    "[medium_violet_red]{}[/medium_violet_red]",
-    "[gold1]{}[/gold1]",
-    "[sea_green1]{}[/sea_green1]",
-    "[white]{}[/white]",
+    "[red]{}[/red]", #0
+    "[purple]{}[/purple]", #1
+    "[dark_orange]{}[/dark_orange]", #2
+    "[chartreuse3]{}[/chartreuse3]", #3
+    "[cyan]{}[/cyan]", #4
+    "[hot_pink]{}[/hot_pink]", #5
+    "[orange4]{}[/orange4]", #6
+    "[dark_cyan]{}[/dark_cyan]", #7
+    "[grey74]{}[/grey74]", #8
+    "[slate_blue3]{}[/slate_blue3]", #9
+    "[medium_violet_red]{}[/medium_violet_red]", #10
+    "[gold1]{}[/gold1]", #11
+    "[sea_green1]{}[/sea_green1]", #12
+    "[white]{}[/white]", #13
+    "[black]{}[/black]", #14
 ]
 
-color_buffer = """{}{}{}{}{}{}{}
-               {}{}{}{}{}{}{}"""
+# Colorful blocks
+# -------------------------------------------------------------------
+BW = colors[13].format(F)
+BC = colors[4].format(F)
+BY = colors[11].format(F)
+BG = colors[3].format(F)
+BR = colors[0].format(F)
+BP = colors[1].format(F)
+BO = colors[2].format(F)
+BK = colors[14].format(F)
+
+COLOR_BANNER = """{}{}{}{}{}{}{}
+           {}{}{}{}{}{}{}"""
+
+# OS logos
+# -------------------------------------------------------------------
+OS_name = distro.id()
+OS_logos = {
+    'nix': ' ',
+    'ubuntu': ' ',
+    'debian': ' ',
+    'raspbian': ' ',
+    'elementary': ' ',
+    'mint': ' ',
+    'centos': ' ',
+    'fedora': ' ',
+    'redhat': ' ',
+    'arch': ' ',
+    'manjaro': ' ',
+    'suse': ' ',
+    'slackware': ' ',
+    'alpine': '',
+    'bsd': ' ',
+    'gentoo': ' ',
+    'darwin': ' ',
+    'macos': ' ',
+    'mac': ' ',
+    'windows': ' ',
+}
 
 # Random Block color list
+# -------------------------------------------------------------------
 block_colors = [BR, BP, BO, BG, BC]
 shuffle(block_colors)
-limit = len(block_colors) + 1
+limit = len(block_colors)
 
 # CPU Brand:
-AMD = '[bold][red]AMD[/red][/bold]:'
-Intel = '[bold][cyan]Intel[/cyan][/bold]'
+# -------------------------------------------------------------------
+AMD = '[red]AMD[/red]:'
+Intel = '[blue]Intel[/blue]'
 
 # System info
-OS = '[bold][red]  OS[/red][/bold]:'
-CPU = '[bold][dark_orange]  Cpu[/dark_orange][/bold]:'
-GPU = '[bold][medium_violet_red]  Gpu[/bold][/medium_violet_red]:'
-RAM = '[bold][slate_blue3]  Ram[/slate_blue3][/bold]:'
-SWAP = '[bold][grey74]  Swap[/grey74][/bold]:'
-DISK = '[bold][hot_pink]  Disk[/hot_pink][/bold]:'
-KERNEL = '[bold][sea_green1]  Kernel[/sea_green1][/bold]:'
-PING = "[bold][gold1]  Ping[/gold1][/bold]:"
-NETWORK = "[bold][dark_cyan]  Network[/dark_cyan][/bold]:"
-UPTIME = "[bold][orange4]  Uptime[/orange4][/bold]:"
-NODE = "[bold][white] {}$ [/white][yellow2] {}[/yellow2][red]@[/red][cyan]{}[/cyan][/bold]"
+# -------------------------------------------------------------------
+system_info_colors = [
+    'red', 'sea_green1', 'dark_orange', 'medium_violet_red', 'slate_blue3',
+    'grey74', 'hot_pink', 'gold1', 'dark_cyan', 'orange4', 'orange4', 
+]
+
+system_info_title = [
+    f"        {OS_logos.get(OS_name, ' ')} OS:",
+    '          Kernel:',
+    '          Cpu:',
+    '          Gpu:',
+    '          Ram:',
+    '          Swap:',
+    '          Disk:',
+    '          Ping:',
+    '          Network:',
+    '          Uptime:',
+]
+
+NODE = "[white]  {}$ [/white][yellow2] {}[/yellow2][red]@[/red][cyan]{}[/cyan]"
 
 # Ghost: Width=29, Height=12
+# -------------------------------------------------------------------
 pacman = """
 ▒▒▒▒▒▒▒██████████████▒▒▒▒▒▒▒▒
 ▒▒▒▒▒███████████████████▒▒▒▒▒
@@ -105,6 +154,7 @@ pacman = """
 """.replace(F, BY)
 
 # Ghost: Width=29, Height=12
+# -------------------------------------------------------------------
 ghost = """
 ▒▒▒▒▒▒▒██████████████▒▒▒▒▒▒▒▒
 ▒▒▒▒▒██████████████████▒▒▒▒▒▒
@@ -120,19 +170,6 @@ ghost = """
 ▒██▒▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒██▒▒
 """
 
-# Ping:
-cmd = 'ping {} 1 8.8.8.8'
-if platform.system() == "Windows":
-    cmd.format('-n')
-elif platform.system() in "Linux Darwin":
-    cmd.format('-c')
-
-ping_proc = subprocess.Popen(
-    cmd.format('-c'),
-    shell=True,
-    stdout=subprocess.PIPE
-)
-
 def clear() -> None:
     if platform.system() in "Linux Darwin":
         subprocess.run(['clear'])
@@ -141,40 +178,39 @@ def clear() -> None:
     else: pass
 
 def cpu() -> str:
-    cpu_info = ""
-    if platform.system() == "Windows":
-        cpu_info = f"{CPU}{platform.processor()}"
-
-    elif platform.system() == "Darwin":
-        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
-        cmd ="sysctl -n machdep.cpu.brand_string"
-        cpu_info = f"{CPU}{subprocess.check_output(cmd).strip()}"
-
-    elif platform.system() == "Linux":
-        cmd = "cat /proc/cpuinfo"
+    cpu_info = f" Platform unsupported!"
+    if platform.system() == 'Linux':
+        cmd = 'cat /proc/cpuinfo'
         all_info = subprocess.check_output(cmd, shell=True).decode().strip()
-        for line in all_info.split("\n"):
-            if "model name" in line:
-                cpu_info = f"{CPU}{''.join(re.sub(r'.*model name.*:', '', line, 1))}".replace('CPU @ ', '')
+        for line in all_info.split('\n'):
+            if 'model name' in line:
+                cpu_info = f"{''.join(re.sub(r'.*model name.*:', '', line, 1))}"\
+                    .replace('CPU @ ', '').strip()
                 break
-    else:
-        cpu_info = f"{CPU} None!"
 
-    return cpu_info + f" {psutil.cpu_count()} Cores"
+    elif platform.system() == 'Darwin':
+        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+        cmd ='sysctl -n machdep.cpu.brand_string'
+        cpu_info = f"{subprocess.check_output(cmd).strip()}"
+
+    elif platform.system() == 'Windows':
+        cpu_info = f"{platform.processor().strip()}"
+
+    return f" {cpu_info} {psutil.cpu_count()} Cores"
 
 def ram() -> str:
     mem = psutil.virtual_memory()
     usage = round(mem.percent)
     used = round(mem.used / (1024 ** 3), 1)
     total = round(mem.total / (1024 ** 3), 1)
-    return f"{RAM} {used} GB  {total} GB usage: {usage}%"
+    return f" {used} GB  {total} GB usage: {usage}%"
 
 def swap() -> str:
     swap = psutil.swap_memory()
     usage = round(swap.percent)
     used = round(swap.used / (1024 ** 3), 1)
     total = round(swap.total / (1024 ** 3), 1)
-    return f"{SWAP} {used} GB  {total} GB usage: {usage}%"
+    return f" {used} GB  {total} GB usage: {usage}%"
 
 def disk() -> str:
     home = psutil.disk_usage('/home')
@@ -185,15 +221,25 @@ def disk() -> str:
     root_total = round(root.total / (1024 ** 3), 1)
     root_free = round(root.free / (1024 ** 3), 1)
     
-    return f"{DISK} Root({root_total} GB) free: {root_free} GB │ Home({home_total} GB) free: {home_free} GB"
+    return f" Root({root_total} GB) free: {root_free} GB │ Home({home_total} GB) free: {home_free} GB"
 
 def ping() -> str:
+    cmd = 'ping {} 1 8.8.8.8'
+    if platform.system() == "Windows":
+        cmd = cmd.format('-n')
+    elif platform.system() in "Linux Darwin":
+        cmd = cmd.format('-c')
+    ping_proc = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE
+    )
     try:
         stdout = ''.join(line.decode('utf-8') for line in ping_proc.stdout)
-        time = re.findall(r'time=.*ms', stdout)[0].replace('time=', '')
-        return f"{PING} {time}  8.8.8.8"
+        time = re.findall(r"time=.*ms", stdout)[0].replace('time=', '')
+        return f" {time}   8.8.8.8"
     except Exception:
-        return f"{PING} 999ms  8.8.8.8"
+        return f" 999ms   8.8.8.8"
 
 def network() -> str:
     if_addrs = psutil.net_if_addrs()
@@ -206,22 +252,71 @@ def network() -> str:
 
     iface_buffer = "{} " * len(iface_addrs)
     iface_buffer = iface_buffer.format(*iface_addrs).strip()
-    return f"{NETWORK} {iface_buffer}"
+    return f" {iface_buffer}"
 
 def gpu() -> str:
-    gpus = GPUtil.getGPUs()
-    if not gpus:
-        return f"{GPU} None!"
-    else:
-        gpu_buffer = "{} " * len(gpus)
-        gpu_buffer = gpu_buffer.format(*[g.name for g in gpus]).strip()
-        return f"{GPU} {gpu_buffer}"
+    gpu_info = ''
+    def finder(brand: Dict[str, List[str]]) -> Generator[str, None, None]:
+        patterns = {
+            'AMD': r"\[.*\].*\[.*\]",
+            'Intel': r"(U?HD Graphics [0-9]*?$)|(Iris Xe Graphics.*)",
+            'NVIDIA': r"\[.*\]"
+        }
+        for name, gpus in brand.items():
+            for gpu in gpus:
+                try:
+                    temp = re.search(patterns.get(name, r".*"), gpu)\
+                        .group().replace('[', '').replace(']', '')
+                except Exception:
+                    yield ""
+                else:
+                    yield f"{name} {temp} │ "
 
-def oper() -> str:
-    return f"{OS} {distro.id().title()} {distro.version()}"
+    # TODO: test gpu info for other os and hardwares.
+    # This is Beta!
+    if platform.system() == 'Linux':
+        stdout = subprocess.run(
+            'lspci',
+            shell=True,
+            stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
+        amd: Dict[str, List[str]] = {'AMD': [
+            *re.findall(r"VGA.*(AMD.*) \(.*", stdout),
+            *re.findall(r"3D.*(AMD.*) \(.*", stdout),
+        ]}
+        intel: Dict[str, List[str]] = {'Intel': [
+            *re.findall(r"VGA.*(Intel.*) \(.*", stdout),
+            *re.findall(r"3D.*(Intel.*) \(.*", stdout),
+        ]}
+        nvidia: Dict[str, List[str]] = {'NVIDIA': [
+            *re.findall(r"VGA.*(NVIDIA.*) \(.*", stdout),
+            *re.findall(r"3D.*(NVIDIA.*) \(.*", stdout),
+        ]}
+
+        for brand in [amd, intel, nvidia]:
+            for gpu in finder(brand):
+                gpu_info += gpu
+        
+        gpu_info = f"{gpu_info.strip('│ ')}"
+
+    elif platform.system() == 'Darwin':
+        gpu_info = 'Can\'t find Gpu and Not implemented'
+        ... # system_profiler SPDisplaysDataType
+
+    elif platform.system() == 'Windows':
+        gpu_info = 'Can\'t find Gpu and Not implemented'
+        ... # wmic path Win32_VideoController get caption
+    
+    else:
+        gpu_info = 'Platform unsupported!'
+
+    return f" {gpu_info}"
+
+def operate() -> str:
+    return f" {OS_name.title()} {distro.version()}"
 
 def kernel() -> str:
-    return f"{KERNEL} {platform.release()}"
+    return f" {platform.release()}"
 
 def node() -> str:
     global D
@@ -245,17 +340,34 @@ def uptime() -> str:
     hours = system_up // 60 // 60
     minutes = system_up // 60 % 60
     seconds = system_up % 60
-    return f"{UPTIME} {hours}:{minutes}:{seconds}"
+    return f" {hours}:{minutes}:{seconds}"
 
-def main(arg: List[str]) -> None:
-    sleep(0.001) # Wait for terminal
+def main() -> None:
+    clear()
+    delay = 0
     console = Console()
     max_width = os.get_terminal_size().columns // (29) # 29 = width of ghost
-    max_ghost =  limit if max_width > limit else max_width # How many ghost can place on terminal
+    max_ghost = limit if max_width >= limit else max_width # How many ghost can place on terminal
 
-    clear()
+    # Argument parsing
+    # -------------------------------------------------------------------
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--version', help="show version", action='store_false', required=False)
+    parser.add_argument('-d', '--delay', help="typewriter style show itself if you set delay <ms>", type=int, required=False)
+    args = parser.parse_args()
+    
+    if args.delay:
+        delay = args.delay
+    elif not args.version:
+        console.print(f"""
+           {choice(colors).format(MAIN_BANNER)}
+           Version: {__version__}
+           Source: {__repo__}
+        """)
+        sys.exit()
 
-    # Draw pacman and ghosts
+    # Draw pacman & ghosts
+    # -------------------------------------------------------------------
     ghost_buffer = "{}" * max_ghost 
     for n in range(H + 1):
         console.print(ghost_buffer.format(
@@ -270,40 +382,38 @@ def main(arg: List[str]) -> None:
                 ),
             )
         )
-        # Argument management
-        if len(arg) == 1:
-            sleep(0.01)
-        elif len(arg) == 2:
-            if arg[1].isdigit():
-                sleep(int(arg[1]) / 1000)
-            else:
-                sleep(0.01)
-        else:
-            sleep(0.01)
+        sleep(delay / 1000)
 
-    # Show system info
-    console.print(
-        f"""
-            {node()}
-            {'─' * D}───────
-            {oper()}
-            {kernel()}
-            {cpu()}
-            {gpu()}
-            {ram()}
-            {swap()}
-            {disk()}
-            {ping()}
-            {network()}
-            {uptime()}
-            {'─' * D}───────
-              {color_buffer.format(*[color.format(F * 3) for color in colors])}
-               {choice(colors).format(BANNER)}
-        """
-    )
+    # Draw system info
+    # -------------------------------------------------------------------
+    system_info_details: List[str] = [
+        operate(), kernel(), cpu(),
+        gpu(), ram(), swap(), disk(),
+        ping(), network(), uptime()
+    ]
+
+    console.print(f"""
+        {node()}
+        {'─' * D}────────""")
+    for color in system_info_colors:
+        try:
+            details = system_info_details.pop(0)
+            title = system_info_title.pop(0)
+        except IndexError: break
+        else:
+            for char in title: # Type titles with color
+                console.print(f"[{color}]{char}[/{color}]", end='')
+                sleep(delay / 1000)
+            for char in details: # Type details without color
+                console.print(f"{char}", end='')
+                sleep(delay / 1000)
+            print()
+    console.print(f"""        {'─' * D}────────
+          {COLOR_BANNER.format(*[color.format(F * 3) for color in colors])}
+           {choice(colors).format(MAIN_BANNER)}""")
 
 if __name__ == '__main__':
     try:
-        main(sys.argv)
+        main()
     except KeyboardInterrupt:
         sys.exit()
